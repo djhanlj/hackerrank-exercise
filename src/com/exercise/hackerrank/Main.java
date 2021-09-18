98b7dbfd8da4de672f8117382f5459ebb7f45a7e
Djair Soares <djhanlj@gmail.com>
2021-09-18 17:41:14 -0300

    Foi utilizado o método reverse da classe Collections para percorrer a lista de numeros, o tamanho da lista foi definido pelo console, e a impressão foi feita concatenando o indice da lista com um espaço em branco.

--- src/com/exercise/hackerrank/Main.java
+++ src/com/exercise/hackerrank/Main.java
@@ -9,27 +9,31 @@ import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
 import java.util.stream.*;
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
         System.out.println("oi tudo bem");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int sizeArray = Integer.parseInt(bufferedReader.readLine().trim());
 
-
-
-
-        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        List<Integer> listNumber = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
                 .collect(toList());
 
         bufferedReader.close();
+
+        Collections.reverse(listNumber);
+        for (int i = 0; i < sizeArray; i++) {
+            System.out.print(listNumber.get(i) + " ");
+        }
+
 
     }
 }
